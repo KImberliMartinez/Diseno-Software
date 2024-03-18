dfbb294cd974226cf454ea276376ff1b7b678346
kikin27z <karimfv04@gmail.com>
2024-03-18 10:07:58 -0700

    Hice las interfaces gráficas
    Cree todas pantallas visuales

---
+++ ElRosal/src/main/java/org/itson/disenosoftware/presentacion/ElRosal.java
@@ -0,0 +1,14 @@
+package org.itson.disenosoftware.presentacion;
+
+/**
+ *
+ * @author karim
+ */
+public class ElRosal {
+
+    public static void main(String[] args) {
+        FrmTipoOrden tipo = new FrmTipoOrden();
+        
+        tipo.setVisible(true);
+    }
+}
